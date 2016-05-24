bec366f1cfe833d9f44aef495620eb2ed8ae9294
Gulzar Ahmed <gulzarhome54@gmail.com>
2016-05-25 02:06:41 +0530

    Tracker client working on background using Google  Updateplay Service

--- app/src/main/java/com/gulzar/locationtracker/BackgroundLocationService.java
+++ app/src/main/java/com/gulzar/locationtracker/BackgroundLocationService.java
@@ -1,10 +1,176 @@
 package com.gulzar.locationtracker;
 
 import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Gulzar on 24-05-2016.
  */
-public class BackgroundLocationService extends Service
-{
+
+public class BackgroundLocationService extends Service implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    IBinder mBinder = new LocalBinder();
+
+    private GoogleApiClient mGoogleApiClient;
+    private String TAG="gulxxx";
+
+    private LocationRequest mLocationRequest;
+
+
+    // Flag that indicates if a request is underway.
+    private boolean mInProgress;
+
+    private Boolean servicesAvailable = false;
+
+    public class LocalBinder extends Binder {
+        public BackgroundLocationService getServerInstance() {
+            return BackgroundLocationService.this;
+        }
+    }
+
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+      buildGoogleApiClient();
+        Log.i(TAG, "onCreate");
+
+        mInProgress = false;
+
+        servicesAvailable = servicesConnected();
+    }
+
+
+    protected synchronized void buildGoogleApiClient() {
+        Log.i(TAG, "Building GoogleApiClient");
+        this.mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    private boolean servicesConnected() {//
+
+        // Check that Google Play services is available
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+
+            return true;
+        } else {
+
+            return false;
+        }
+    }
+
+    public int onStartCommand (Intent intent, int flags, int startId)
+    {
+        super.onStartCommand(intent, flags, startId);
+        Log.i(TAG, "onStart");
+
+        if(!servicesAvailable || mGoogleApiClient.isConnected() || mInProgress)
+            return START_STICKY;
+        buildGoogleApiClient();
+        if(!mGoogleApiClient.isConnected() || !mGoogleApiClient.isConnecting() && !mInProgress)
+        {
+            mInProgress = true;
+            mGoogleApiClient.connect();
+        }
+
+        return START_STICKY;
+    }
+
+
+    // Define the callback method that receives location updates
+    @Override
+    public void onLocationChanged(Location location) {
+        // Report to the UI that the location was updated
+        String msg = Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Log.d("debug", msg);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    public String getTime() {
+        SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return mDateFormat.format(new Date());
+    }
+
+
+
+    @Override
+    public void onDestroy(){
+        mInProgress = false;
+        // Disconnecting the client invalidates it.
+        mGoogleApiClient.disconnect();
+        super.onDestroy();
+    }
+
+    /*
+     * Called by Location Services when the request to connect the
+     * client finishes successfully. At this point, you can
+     * request the current location or start periodic updates
+     */
+
+    @Override
+    @SuppressWarnings({"MissingPermission"})
+    public void onConnected(Bundle bundle) {
+
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000); // Update location every second
+
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+    /*
+     * Called by Location Services if the attempt to
+     * Location Services fails.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        mInProgress = false;
+       Log.i(TAG, "GoogleApiClient connection has failed");
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "GoogleApiClient connection has been suspend");
+    }
+
+
 }
--- app/src/main/java/com/gulzar/locationtracker/MainActivity.java
+++ app/src/main/java/com/gulzar/locationtracker/MainActivity.java
@@ -3,13 +3,26 @@ package com.gulzar.locationtracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mServiceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this,BackgroundLocationService.class));
+        //startService(new Intent(getBaseContext(),BackgroundLocationService.class));
+        mServiceButton=(Button)findViewById(R.id.startservice);
+
+    }
+    public void clicked(View view)
+    {
+
+            startService(new Intent(getBaseContext(),BackgroundLocationService.class));
+            Toast.makeText(getBaseContext(),"Service started",Toast.LENGTH_SHORT).show();
     }
 }
