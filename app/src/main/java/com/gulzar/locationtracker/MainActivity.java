6c63d7460a028289379a28a6c13730aa783ff0e8
Gulzar Ahmed <gulzarhome54@gmail.com>
2016-05-25 17:53:59 +0530

    Snackbar displays connectivity issue and GPS asks to eanble GPS

--- app/src/main/java/com/gulzar/locationtracker/MainActivity.java
+++ app/src/main/java/com/gulzar/locationtracker/MainActivity.java
@@ -1,15 +1,21 @@
 package com.gulzar.locationtracker;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.FirebaseDatabase;
@@ -44,18 +50,90 @@ public class MainActivity extends AppCompatActivity {
             if(mUID.getText()==null||mUID.getText().length()!=7)
                Toast.makeText(getBaseContext(),"Enter correct ID",Toast.LENGTH_SHORT).show();
                 else{
-                if(checkInternetConnectivity(getBaseContext()))
+                //Check INTERNET CONNECT AND GPS CONNECTIVITY
+                if(checkInternetConnectivity(getBaseContext()) && checkGPSConnectivity())
                 {
             SaveToSharedPreference(mUID.getText().toString());
             startService(new Intent(getBaseContext(),BackgroundLocationService.class));
             Toast.makeText(getBaseContext(),"Service started",Toast.LENGTH_SHORT).show();
                 }
-                else
-                    Toast.makeText(getBaseContext(),"NO INTERNET CONNECTION",Toast.LENGTH_SHORT).show();
+                else if(!checkInternetConnectivity(getBaseContext()))
+                    displaySnackBarNoConnectivity();
+                     if(!checkGPSConnectivity())
+                         showGPSDisabledAlertToUser();
 
             }
     }
 
+    private void showGPSDisabledAlertToUser() {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("GPS is disabled in your device. Would you like to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Goto Settings Page To Enable GPS",
+                        new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int id){
+                                Intent callGPSSettingIntent = new Intent(
+                                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(callGPSSettingIntent);
+                            }
+                        });
+        alertDialogBuilder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+
+    }
+
+    private boolean checkGPSConnectivity() {
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            Toast.makeText(this, "GPS ENABLED", Toast.LENGTH_SHORT).show();
+            return true;
+        }else{
+            return false;
+
+        }
+
+    }
+
+    /**
+     * Displays message when internet connectvity is absent
+     */
+    private void displaySnackBarNoConnectivity() {
+
+        Snackbar snackbar = Snackbar
+                .make(findViewById(R.id.layout_rel), "No internet connection!", Snackbar.LENGTH_LONG)
+                .setAction("RETRY", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
+
+// Changing message text color
+        snackbar.setActionTextColor(Color.RED);
+
+// Changing action button text color
+        View sbView = snackbar.getView();
+        TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setTextColor(Color.YELLOW);
+        snackbar.show();
+    }
+
+
+
+    /**
+     * To check whether the application is connected to the net
+     * @param context
+     * @return boolean
+     */
     private boolean checkInternetConnectivity(Context context) {
         ConnectivityManager cm =
                 (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
